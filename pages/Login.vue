0e5e88493bd95fce086456dc8eee6d94b8782d99
Makarios Vardaxis <salonikamakarios@gmail.com>
2023-10-08 23:47:00 +0300

    styling

--- pages/Login.vue
+++ pages/Login.vue
@@ -18,14 +18,14 @@
                             </div>
                         </div>
                         <form @submit.prevent="checkForm" id="Login">
-                            <div class="form-group row">
+                            <div class="form-group row mb-2">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
                                 <div class="col-md-6">
                                     <input id="email" v-model="emailInput" type="email" class="form-control" name="email" required
                                         autocomplete="email" autofocus>
                                 </div>
                             </div>
-                            <div class="form-group row">
+                            <div class="form-group row mb-2">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">Κωδικός
                                     πρόσβασης</label>
                                 <div class="col-md-6">
