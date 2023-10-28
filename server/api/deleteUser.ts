622a71b6e63529ff31fa245f3cd8cecda7ad7243
Makarios Vardaxis <salonikamakarios@gmail.com>
2023-10-29 02:07:12 +0300

    Create deleteUser.ts

---
+++ server/api/deleteUser.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const { id } = getQuery(event);
+  const { auth_token } = getQuery(event);
+
+  try {
+    const response = await axios.delete(`${config.public.apiUrl}/user/${auth_token}`);
+    const deleteResponse = response.data;
+    return {
+        deleteResponse: deleteResponse,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+});
